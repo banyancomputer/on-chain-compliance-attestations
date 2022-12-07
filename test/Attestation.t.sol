85c00e3ae2698556bcb388db0bd401a66853aa09
Xiangan He <xiangan9@gmail.com>
2022-12-07 15:37:29 -0500

    some tests for attestation

--- test/Attestation.t.sol
+++ test/Attestation.t.sol
@@ -6,14 +6,29 @@ import "../src/Attestor.sol";
 
 // TODO some more work here...
 contract AttestationTest is Test {
+
     Attestor public attestor;
     function setUp() public {
-        //TODO
-        // attestor = new Attestor("Test", );
+        // Bug/ToFIX: should require cid / data to not be zero on init
+        CIDOrBytes memory testCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
+        attestor = new Attestor("Test", testCidOrBytes);
     }
 
-    function testIncrement() public {
-        //NotImplmented
+    function test_getAttestorInfo() public {
+        CIDOrBytes memory verifyCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
+        assertEq(attestor.getAttestorInfo(), verifyCidOrBytes);
+    }
+
+    function test_setAttestorInfo() public {
+        CIDOrBytes memory settingCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
+        //TOFIX: should not be able to set the same CidOrBytes as before
+        assertEq(attestor.getAttestorInfo(), settingCidOrBytes);
+    }
+
+    function test_issueAttestation() public {
+        Attestation memory attestation = Attestation({subject: address(0), expiration: 1, identifier: "test", associated_docs: CIDOrBytes({isCID:true, cid:"0x", data:"0x"})});
+        CIDOrBytes memory verifyCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
+        vm.expectRevert(address(0), attestation);
     }
 
     function testSetNumber(uint256 x) public {
