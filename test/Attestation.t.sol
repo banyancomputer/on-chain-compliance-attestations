567677a7e4bdda1e90e3eff2737640e8a1444c21
Xiangan He <xiangan9@gmail.com>
2022-12-22 09:35:13 -0500

    finished, initial tests

--- test/Attestation.t.sol
+++ test/Attestation.t.sol
@@ -3,35 +3,47 @@ pragma solidity ^0.8.17;
 
 import "@forge-std/Test.sol";
 import "../src/Attestor.sol";
-
+import "./utils/Utilities.sol";
+    
 // TODO some more work here...
 contract AttestationTest is Test {
 
+    Utilities internal utils;
     Attestor public attestor;
+    address payable[] users;
+
     function setUp() public {
         // Bug/ToFIX: should require cid / data to not be zero on init
         CIDOrBytes memory testCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
         attestor = new Attestor("Test", testCidOrBytes);
+        users = utils.createUsers(2, 100 ether);
     }
 
-    function test_getAttestorInfo() public {
-        CIDOrBytes memory verifyCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
-        assertEq(attestor.getAttestorInfo(), verifyCidOrBytes);
-    }
-
-    function test_setAttestorInfo() public {
+    function test_getsetAttestorInfo() public {
         CIDOrBytes memory settingCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
         //TOFIX: should not be able to set the same CidOrBytes as before
-        assertEq(attestor.getAttestorInfo(), settingCidOrBytes);
+        attestor.setAttestorInfo(settingCidOrBytes);
+        assertEq(attestor.getAttestorInfo().isCID == true, settingCidOrBytes.isCID == true);
+        vm.expectRevert();
+        attestor.setAttestorInfo(settingCidOrBytes);
     }
 
     function test_issueAttestation() public {
-        Attestation memory attestation = Attestation({subject: address(0), expiration: 1, identifier: "test", associated_docs: CIDOrBytes({isCID:true, cid:"0x", data:"0x"})});
-        CIDOrBytes memory verifyCidOrBytes = CIDOrBytes({isCID:true, cid:"0x", data:"0x"});
-        vm.expectRevert(address(0), attestation);
+        Attestation memory attestation = Attestation({subject: users[0], expiration: 1, identifier: 'test', associated_docs: CIDOrBytes({isCID:true, cid:"0x", data:"0x"}) });
+        CIDOrBytes memory verifyCidOrBytes = CIDOrBytes({isCID:true, cid:'0x', data:"0x"});
+        uint attestationID = attestor.issueAttestation(users[0], attestation);
+        vm.expectRevert();
+        attestor.issueAttestation(address(0), attestation);
+        vm.expectRevert();
+        attestor.issueAttestation(users[0], attestation);
+        bool testExpected = false;
+        (testExpected, attestation, testExpected) = attestor.getAttestation(attestationID);
+        assertEq(attestor.getAttestationExpiration(attestationID)==1, true);
+        attestor.extendAttestation(attestationID, 2);
+        assertEq(attestor.getAttestationExpiration(attestationID)==2, true);
+        vm.expectRevert();
+        attestor.extendAttestation(attestationID, 2);
+        attestor.appendDocToAttestation(attestationID, verifyCidOrBytes);
     }
 
-    function testSetNumber(uint256 x) public {
-        //NotImplemented
-    }
 }
